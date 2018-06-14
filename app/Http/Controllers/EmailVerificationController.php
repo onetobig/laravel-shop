050c3ad5927739bbb77c66d1dc8323e413bd9440
onetobig <onetobig@gmail.com>
2018-06-14 13:42:35 +0800

    触发激活邮件

---
+++ app/Http/Controllers/EmailVerificationController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Notifications\EmailVerificationNotification;
+use Illuminate\Http\Request;
+use \Exception;
+use Cache;
+
+class EmailVerificationController extends Controller
+{
+    public function verify(Request $request)
+    {
+        $email = $request->input('email');
+        $token = $request->input('token');
+        if (!$email || !$token) {
+            throw new Exception('验证链接不正确');
+        }
+
+        $key = 'email_verification_' . $email;
+        if ($token != Cache::get($key)) {
+            throw new Exception('验证链接不存在或已过期');
+        }
+
+        if (!$user = User::where('email', $email)->first()) {
+            throw new Exception('用户不存在');
+        }
+
+        Cache::forget($key);
+        $user->update([
+            'email_verified' => true,
+        ]);
+
+        return view('pages.success', ['msg' => '邮箱验证成功']);
+    }
+
+    public function send(Request $request)
+    {
+        $user = $request->user();
+
+        if ($user->eamil_verified) {
+            throw new Exception('您已经激活过邮箱了');
+        }
+
+        $user->notify(new EmailVerificationNotification());
+
+        return view('pages.success', ['msg' => '邮件发送成功']);
+    }
+}

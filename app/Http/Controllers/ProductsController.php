1f710875e397794e9e0b3e996c5a8a0dd343f4fa
onetobig <onetobig@gmail.com>
2018-06-19 10:27:31 +0800

    用户界面 - 商品筛选、排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,13 +4,36 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-        $products = Product::query()->where('on_sale', true)->paginate(16);
+        $builder = Product::query()->where('on_sale', true);
 
-        return view('products.index', compact('products'));
+        if ($search = $request->input('search', '')) {
+            $like = '%' . $search . '%';
+            $builder->where(function ($query) use ($like)  {
+                $query->where('title', 'like', $like)
+                    ->orWhere('description', 'like', $like)
+                    ->orWhereHas('skus', function($query) use ($like) {
+                        $query->where('title', 'like', $like)
+                            ->orWhere('description', 'like', $like);
+                    });
+            });
+        }
+
+        if($order = $request->input('order', ''))  {
+            if( preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                if ( in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+        $products = $builder->paginate(16);
+
+        $filters = compact('search', 'order');
+        return view('products.index', compact('products', 'filters'));
     }
 }

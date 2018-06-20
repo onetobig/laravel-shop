7a31aac4e4bdb53593438ed6fe06a490dd495bad
onetobig <onetobig@gmail.com>
2018-06-20 09:46:29 +0800

    创建订单

---
+++ app/Http/Requests/OrderRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\ProductSku;
+use Illuminate\Validation\Rule;
+
+class OrderRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'address_id' => ['required', Rule::exists('user_addresses', 'id')->where('user_id', $this->user()->id)],
+            'items' => ['required', 'array'],
+            'items.*.sku_id' => [  // 检查 items 数组下每一个子数组的 sku_id 参数
+                'required',
+                function ($attribute, $value, $fail) {
+                    if (!$sku = ProductSku::find($value)) {
+                        $fail('该商品不存在');
+                    }
+                    if (!$sku->product->on_sale) {
+                        $fail('该商品未上架');
+                    }
+                    if($sku->stock === 0) {
+                        $fail('该商品已售完');
+                    }
+
+                    // 获取当前索引
+                    preg_match('/items\.(\d+)\.sku_id/', $attribute, $m);
+                    $index = $m[1];
+                    $amount = $this->input('items')[$index]['amount'];
+                    if(is_numeric($amount) && $amount > $sku->stock) {
+                        $fail('该商品库存不足');
+                    }
+                }
+            ],
+            'items.*.amount' => ['required', 'integer', 'min:1'],
+        ];
+    }
+}

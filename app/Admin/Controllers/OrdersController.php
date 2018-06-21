a4e9cb7c4be34d3aafefde2af37846887efa1a72
onetobig <onetobig@gmail.com>
2018-06-21 10:00:20 +0800

    管理后台 - 订单发货

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Admin\Controllers;
 
+use App\Exceptions\InvalidRequestException;
 use App\Models\Order;
 
 use Encore\Admin\Form;
@@ -11,6 +12,7 @@ use Encore\Admin\Layout\Content;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\ModelForm;
 use function foo\func;
+use Illuminate\Http\Request;
 
 class OrdersController extends Controller
 {
@@ -124,5 +126,34 @@ class OrdersController extends Controller
             $form->display('created_at', 'Created At');
             $form->display('updated_at', 'Updated At');
         });
+    }
+
+    public function ship(Order $order, Request $request)
+    {
+        // 判断是否已支付
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未付款');
+        }
+
+        // 判断当前订单是否未发货
+        if ($order->ship_status !== Order::SHIP_STATUS_PENDING) {
+            throw new InvalidRequestException('该订单已发货');
+        }
+
+        $data = $this->validate($request, [
+            'express_company' => ['required'],
+            'express_no' => ['required'],
+        ], [], [
+            'express_company' => '物流公司',
+            'express_no' => '物流单号',
+        ]);
+
+        // 将订单发货状态改为已发货，并存入物流信息
+        $order->update([
+            'ship_status' => Order::SHIP_STATUS_DELIVERED,
+            'ship_data' => $data,
+        ]);
+
+        return redirect()->back();
     }
 }

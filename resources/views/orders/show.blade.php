f41c1f18283f1d4fac13750b2632e302cd4849a5
onetobig <onetobig@gmail.com>
2018-06-21 10:57:03 +0800

    确认收货

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -45,6 +45,16 @@
 							<div class="line"><div class="line-label">收货地址：</div><div class="line-value">{{ join(' ', $order->address) }}</div></div>
 							<div class="line"><div class="line-label">订单备注：</div><div class="line-value">{{ $order->remark ?: '-'}}</div></div>
 							<div class="line"><div class="line-label">订单编号：</div><div class="line-value">{{ $order->no }}</div></div>
+							<div class="line">
+								<div class="line-label">物流状态：</div>
+								<div class="line-value">{{ \App\Models\Order::$shipStatusMap[$order->ship_status] }}</div>
+							</div>
+							@if($order->ship_data)
+								<div class="line">
+									<div class="line-label">物流信息：</div>
+									<div class="line-value">{{ $order->ship_data['express_company'] }} {{ $order->ship_data['express_no'] }}</div>
+								</div>
+							@endif
 						</div>
 						<div class="order-summary text-right">
 							<div class="total-amount">
@@ -73,6 +83,12 @@
 									<button class="btn btn-success btn-sm" id="btn-wechat">微信支付</button>
 								</div>
 							@endif
+							
+							@if( $order->ship_status === \App\Models\Order::SHIP_STATUS_DELIVERED )
+								<div class="receive-button">
+										<button class="btn btn-sm btn-success" id="btn-receive" type="button">确认收货</button>
+								</div>
+							@endif
 						</div>
 					</div>
 				</div>
@@ -83,6 +99,22 @@
 @section('scriptAfterJs')
 	<script>
 		$(document).ready(function(){
+		    $('#btn-receive').click(function () {
+		        swal({
+			        title: '确认已经收到商品？',
+			        icon: 'warning',
+			        dangerMode: true,
+			        buttons: ['取消',  '确认收到']
+		        }).then(function (ret) {
+		            if (!ret) {
+		                return;
+		            }
+		            axios.post('{{ route('orders.receive', ['order' => $order->id]) }}')
+			            .then(function () {
+			                location.reload();
+			            })
+		        })
+		    });
 		    $('#btn-wechat').click(function() {
 		        swal({
 			        content: $('<img src="{{ route('payment.wechat', ['order' => $order->id]) }}"/>')[0],
@@ -97,5 +129,6 @@
 		    });
 			   
 		});
+		
 	</script>
 @endsection

b83edd85157e75403d8fa8e7c9a284f8a5abd27d
onetobig <onetobig@gmail.com>
2018-07-10 08:50:08 +0000

    用户界面 - 商品筛选、排序

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -16,7 +16,7 @@ class Product extends Model
         'on_sale' => 'boolean',
     ];
 
-    public function sku()
+    public function skus()
     {
         return $this->hasMany(ProductSku::class);
     }

da19e384df8f51818153a9a609fc351ee15ef9ca
onetobig <onetobig@gmail.com>
2018-07-11 09:37:41 +0800

    用户界面 - 商品收藏

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Auth::routes();
 
@@ -29,6 +30,7 @@ Route::group(['middleware' => 'auth'], function() {
         Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
         Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
-        Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+        Route::post('products/{product}/favor', 'ProductsController@favor')->name('products.favor');
+        Route::post('products/{product}/disfavor', 'ProductsController@disfavor')->name('products.disfavor');
     });
 });

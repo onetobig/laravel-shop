2621071bced94bbb688975e366ee0b40cdbf625f
onetobig <onetobig@gmail.com>
2018-07-18 15:03:29 +0800

    管理后台 - 新增、编辑商品

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -38,10 +38,7 @@ class ProductsController extends Controller
     public function edit($id)
     {
         return Admin::content(function (Content $content) use ($id) {
-
-            $content->header('header');
-            $content->description('description');
-
+            $content->header('编辑商品');
             $content->body($this->form()->edit($id));
         });
     }
@@ -55,9 +52,7 @@ class ProductsController extends Controller
     {
         return Admin::content(function (Content $content) {
 
-            $content->header('header');
-            $content->description('description');
-
+            $content->header('创建商品');
             $content->body($this->form());
         });
     }
@@ -103,9 +98,20 @@ class ProductsController extends Controller
         return Admin::form(Product::class, function (Form $form) {
 
             $form->display('id', 'ID');
+            $form->text('title', '商品名称')->rules('required');
+            $form->image('image', '封面图片')->rules('required');
+            $form->editor('description', '商品描述')->rules('required');
+            $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default('0');
+            $form->hasMany('skus', 'SKU 列表', function (Form\NestedForm $form) {
+                $form->text('title', 'SKU 名称')->rules('required');
+                $form->text('description', 'SKU 描述')->rules('required');
+                $form->text('price', '单价')->rules('required|numeric|min:0.01');
+                $form->text('stock', '库存')->rules('required|integer|min:0');
+            });
 
-            $form->display('created_at', 'Created At');
-            $form->display('updated_at', 'Updated At');
+            $form->saving(function (Form $form) {
+                $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
+            });
         });
     }
 }

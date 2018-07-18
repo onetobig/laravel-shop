ca3981c7d56841789b3b21fdc9c7e34858950fbd
onetobig <onetobig@gmail.com>
2018-07-18 15:56:48 +0800

    用户界面 - 商品筛选、排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,9 +7,35 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::query()->where('on_sale', true)->paginate(16);
-        return view('products.index', compact('products'));
+        $builder = Product::query();
+
+        if ($search = $request->search) {
+            $like = "%{$search}%";
+            $builder->where(function ($query) use($like) {
+                $query->where('title', 'like', $like)
+                    ->orWhere('description', 'like', $like)
+                    ->orWhereHas('skus', function ($query) use ($like) {
+                        $query->where('title', 'like', $like)
+                            ->orWhere('description', 'like', $like);
+                    });
+            });
+        }
+
+        if ($order = $request->order) {
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+        $products = $builder->paginate(16);
+
+        $filters = [
+            'order' => $order,
+            'search' => $search,
+        ];
+        return view('products.index', compact('products', 'filters'));
     }
 }

51797fbf9c4f72d8d847e01c1f16de8128d7a1f0
onetobig <onetobig@gmail.com>
2018-07-23 15:09:43 +0800

    完善支付后逻辑

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\OrderPaid;
+use App\Exceptions\InvalidRequestException;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    public function payByAlipay(Order $order, Request $request)
+    {
+        $this->authorize('own', $order);
+        if ($order->paid_at || $order->closed) {
+            throw new InvalidRequestException('订单状态不正确');
+        }
+
+        return app('alipay')->web([
+            'out_trade_no' => $order->no,
+            'total_amount' => $order->total_amount,
+            'subject'      => '支付 Laravel Shop 的订单：' . $order->no,
+        ]);
+    }
+
+    public function alipayReturn()
+    {
+        try {
+            app('alipay')->verify();
+        } catch (\Exception $e) {
+            throw new InvalidRequestException('数据不正确');
+        }
+        return view('pages.success', ['msg' => '付款成功']);
+    }
+
+    public function alipayNotify()
+    {
+        $data = app('alipay')->verify();
+        $order = Order::where('no', $data->out_trade_no)->first();
+        if (!$order) {
+            return 'fail';
+        }
+        if ($order->paid_at) {
+            return app('alipay')->success();
+        }
+
+        $order->update([
+            'paid_at'        => now(),
+            'payment_method' => 'alipay',
+            'payment_no'     => $data->trade_no,
+        ]);
+
+        $this->afterPaid($order);
+        return app('alipay')->success();
+    }
+
+    protected function afterPaid(Order $order)
+    {
+        event(new OrderPaid($order));
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Events\OrderPaid;
+use App\Listeners\SendOrderPaidMail;
 use App\Listeners\SendVerificationEmailToUser;
+use App\Listeners\UpdateProductSoldCount;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -17,7 +20,11 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendVerificationEmailToUser::class,
-        ]
+        ],
+        OrderPaid::class => [
+            UpdateProductSoldCount::class,
+            SendOrderPaidMail::class,
+        ],
     ];
 
     /**

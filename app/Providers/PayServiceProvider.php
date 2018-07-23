f517aedad750c8eb404e3e7ec7f210fd133c0684
onetobig <onetobig@gmail.com>
2018-07-23 14:04:29 +0800

    集成支付宝

--- app/Providers/PayServiceProvider.php
+++ app/Providers/PayServiceProvider.php
@@ -28,7 +28,7 @@ class PayServiceProvider extends ServiceProvider
         $this->app->singleton('alipay', function() {
             $config = config('pay.alipay');
             if (app()->environment() !== 'production') {
-                $config['model'] = 'dev';
+                $config['mode'] = 'dev';
                 $config['log']['level'] = Logger::DEBUG;
             } else {
                 $config['log']['level'] = Logger::WARNING;
--- config/pay.php
+++ config/pay.php
@@ -2,21 +2,21 @@
 
 return [
     'alipay' => [
-        'app_id'         => '',
-        'ali_public_key' => '',
-        'private_key'    => '',
-        'log'            => [
+        'app_id' => env('ALI_APP_ID'),
+        'ali_public_key' => env('ALI_PUB_KEY'),
+        'private_key' => env('ALI_PRI_KEY'),
+        'log' => [
             'file' => storage_path('logs/alipay.log'),
         ],
     ],
 
     'wechat' => [
-        'app_id'      => '',
-        'mch_id'      => '',
-        'key'         => '',
+        'app_id' => '',
+        'mch_id' => '',
+        'key' => '',
         'cert_client' => '',
-        'cert_key'    => '',
-        'log'         => [
+        'cert_key' => '',
+        'log' => [
             'file' => storage_path('logs/wechat_pay.log'),
         ],
     ],

d5e21ed6c26c16a7a488539a89e6a617841475e2
onetobig <onetobig@gmail.com>
2018-07-24 16:54:15 +0800

    完善假数据填充

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -1,9 +1,44 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Models\User;
+use App\Models\Order;
+use App\Models\CouponCode;
 
 $factory->define(App\Models\Order::class, function (Faker $faker) {
+    $user = User::query()->inRandomOrder()->first();
+    $address = $user->addresses()->inRandomOrder()->first();
+    $refund = random_int(0, 9) < 1;
+    $ship = $faker->randomElement(array_keys(Order::$shipStatusMap));
+    $coupon = null;
+    if (random_int(0, 9) < 3) {
+        $coupon = CouponCode::query()->where('min_amount', 0)->inRandomOrder()->first();
+        $coupon->changeUsed();
+    }
+
     return [
-        //
+        'address'        => [
+            'address'       => $address->full_address,
+            'zip'           => $address->zip,
+            'contact_name'  => $address->contact_name,
+            'contact_phone' => $address->contact_phone,
+        ],
+        'total_amount'   => 0,
+        'remark'         => $faker->sentence,
+        'paid_at'        => $faker->dateTimeBetween('-30 days'),
+        'payment_method' => $faker->randomElement(['wechat', 'alipay']),
+        'payment_no'     => $faker->uuid,
+        'refund_status'  => $refund ? Order::REFUND_STATUS_SUCCESS : Order::REFUND_STATUS_PENDING,
+        'refund_no'      => $refund ? Order::getAvailableRefundNo() : null,
+        'closed'         => false,
+        'reviewed'       => random_int(0, 9) > 2,
+        'ship_status'    => $ship,
+        'ship_data'      => $ship === Order::SHIP_STATUS_PENDING ? null : [
+            'express_company' => $faker->company,
+            'express_no'      => $faker->uuid,
+        ],
+        'extra'          => $refund ? ['refund_reason' => $faker->sentence] : [],
+        'user_id'        => $user->id,
+        'coupon_code_id' => $coupon ? $coupon->id : null,
     ];
 });

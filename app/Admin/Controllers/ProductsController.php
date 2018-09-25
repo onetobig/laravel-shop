8733fc5239195c80a52e56eab41d17accaf3d788
onetobig <onetobig@gmail.com>
2018-09-25 17:40:50 +0800

    管理后台商品控制器封装

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -10,117 +10,31 @@ use Encore\Admin\Grid;
 use Encore\Admin\Facades\Admin;
 use Encore\Admin\Layout\Content;
 use App\Http\Controllers\Controller;
-use Encore\Admin\Controllers\ModelForm;
 
-class ProductsController extends Controller
+class ProductsController extends CommonProductsController
 {
-    use ModelForm;
 
-    /**
-     * Index interface.
-     *
-     * @return Content
-     */
-    public function index()
+    public function getProductType()
     {
-        return Admin::content(function (Content $content) {
-
-            $content->header('商品列表');
-            $content->body($this->grid());
-        });
+        return Product::TYPE_NORMAL;
     }
 
-    /**
-     * Edit interface.
-     *
-     * @param $id
-     * @return Content
-     */
-    public function edit($id)
+    protected function customGrid(Grid $grid)
     {
-        return Admin::content(function (Content $content) use ($id) {
-            $content->header('编辑商品');
-            $content->body($this->form()->edit($id));
+        $grid->model()->with(['category']);
+        $grid->id('ID')->sortable();
+        $grid->title('商品名称');
+        $grid->column('category.name', '类目');
+        $grid->on_sale('已上架')->display(function ($value) {
+            return $value ? '是' : '否';
         });
+        $grid->price('价格');
+        $grid->rating('评分');
+        $grid->sold_count('销量');
+        $grid->review_count('评论数');
     }
 
-    /**
-     * Create interface.
-     *
-     * @return Content
-     */
-    public function create()
+    protected function customForm(Form $form)
     {
-        return Admin::content(function (Content $content) {
-
-            $content->header('创建商品');
-            $content->body($this->form());
-        });
-    }
-
-    /**
-     * Make a grid builder.
-     *
-     * @return Grid
-     */
-    protected function grid()
-    {
-        return Admin::grid(Product::class, function (Grid $grid) {
-            $grid->model()->where('type', Product::TYPE_NORMAL)->with(['category']);
-            $grid->id('ID')->sortable();
-            $grid->title('商品名称');
-            $grid->column('category.name', '类目');
-            $grid->on_sale('已上架')->display(function ($value) {
-                return $value ? '是' : '否';
-            });
-            $grid->price('价格');
-            $grid->rating('评分');
-            $grid->sold_count('销量');
-            $grid->review_count('评论数');
-
-            $grid->actions(function ($actions) {
-                $actions->disableDelete();
-            });
-
-            $grid->tools(function ($tools) {
-                $tools->batch(function ($batch) {
-                    $batch->disableDelete();
-                });
-            });
-        });
-    }
-
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
-    protected function form()
-    {
-        return Admin::form(Product::class, function (Form $form) {
-
-            $form->hidden('type')->value(Product::TYPE_NORMAL);
-            $form->display('id', 'ID');
-            $form->text('title', '商品名称')->rules('required');
-            $form->select('category_id', '类目')->options(function ($id) {
-                $category = Category::find($id);
-                if ($category) {
-                    return [$category->id => $category->full_name];
-                }
-            })->ajax('/admin/api/categories?is_directory=0');
-            $form->image('image', '封面图片')->rules('required');
-            $form->editor('description', '商品描述')->rules('required');
-            $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default('0');
-            $form->hasMany('skus', 'SKU 列表', function (Form\NestedForm $form) {
-                $form->text('title', 'SKU 名称')->rules('required');
-                $form->text('description', 'SKU 描述')->rules('required');
-                $form->text('price', '单价')->rules('required|numeric|min:0.01');
-                $form->text('stock', '库存')->rules('required|integer|min:0');
-            });
-
-            $form->saving(function (Form $form) {
-                $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
-            });
-        });
     }
 }

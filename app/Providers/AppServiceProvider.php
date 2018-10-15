80f159e2f043e50336c8820e5528a610a92e3a48
onetobig <onetobig@gmail.com>
2018-10-15 15:55:25 +0800

    引入 Elasticsearch

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
+use Elasticsearch\ClientBuilder as ESClientBuilder;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton('es', function () {
+            $builder = ESClientBuilder::create()->setHosts(config('database.elasticsearch.hosts'));
+            if (app()->environment() === 'local') {
+                $builder->setLogger(app('log')->getMonolog());
+            }
+            return $builder->build();
+        });
     }
 }
--- config/database.php
+++ config/database.php
@@ -117,4 +117,7 @@ return [
 
     ],
 
+    'elasticsearch' => [
+        'hosts' => explode(',', env('ES_HOSTS')),
+    ]
 ];

ddd8e78d6d9cd3d813db1932a57018a70a845926
onetobig <onetobig@gmail.com>
2018-10-22 15:50:50 +0800

    使用 Elasticsearch 重构商品搜索功能

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -8,52 +8,95 @@ use App\Models\OrderItem;
 use App\Models\Product;
 use App\Services\CategoryService;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ProductsController extends Controller
 {
     public function index(Request $request, CategoryService $categoryService)
     {
-        $builder = Product::query()->where('on_sale', true);
+        $page = $request->input('page', 1);
+        $perPage = 16;
 
-        if ($search = $request->search) {
-            $like = "%{$search}%";
-            $builder->where(function ($query) use($like) {
-                $query->where('title', 'like', $like)
-                    ->orWhere('description', 'like', $like)
-                    ->orWhereHas('skus', function ($query) use ($like) {
-                        $query->where('title', 'like', $like)
-                            ->orWhere('description', 'like', $like);
-                    });
-            });
-        }
+        // 查询基本结构
+        $params = [
+            'index' => 'products',
+            'type' => '_doc',
+            'body' => [
+                'from' => ($page - 1) * $perPage,
+                'size' =>  $perPage,
+                'query' => [
+                    'bool' => [
+                        'filter' => [
+                            ['term' => ['on_sale' => true]],
+                        ],
+                    ],
+                ],
+            ],
+        ];
 
-        // 类目搜索
-        if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
-            if ($category->is_directory) {
-                $builder->whereHas('category', function ($query) use ($category) {
-                    $query->where('path', 'like', $category->path . $category->id . '-');
-                });
-            } else {
-                $builder->where('category_id', $category->id);
-            }
-        }
-
-        if ($order = $request->order) {
+        // 价格等排序
+        if ($order = $request->input('order', '')) {
             if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
                 if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
-                    $builder->orderBy($m[1], $m[2]);
+                    $params['body']['sort'] = [[$m[1] => $m[2]]];
                 }
             }
         }
-        $products = $builder->paginate(16);
 
-        $filters = [
-            'order' => $order,
-            'search' => $search,
-        ];
-        $category = $category ?? null;
-        $categoryTree = $categoryService->getCategoryTree();
-        return view('products.index', compact('products', 'filters', 'category', 'categoryTree'));
+        // 按类目检索
+        if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
+            if ($category->is_directory) {
+                $params['body']['query']['bool']['filter'][] = [
+                    'prefix' => ['category_path' => $category->path. $category->id . '-'],
+                ];
+            } else {
+                $params['body']['query']['bool']['filter'][] = ['term' => ['category_id' => $category->id]];
+            }
+        }
+
+        // 关键字搜索
+        if ($search = $request->input('search', '')) {
+            $keywords = array_filter(explode(' ', $search));
+            $params['body']['query']['bool']['must'] = [];
+            foreach ($keywords as $keyword) {
+                $params['body']['query']['bool']['must'][] = [
+                    'multi_match' => [
+                        'query' => $keyword,
+                        'fields' => [
+                            'title^3',
+                            'long_title^2',
+                            'category^2',
+                            'description',
+                            'skus_title',
+                            'skus_description',
+                            'properties.value',
+                        ],
+                    ],
+                ];
+            }
+        }
+
+        $result = app('es')->search($params);
+
+        $productIds = collect($result['hits']['hits'])->pluck('_id')->all();
+        $products = Product::query()
+            ->whereIn('id', $productIds)
+            ->orderByRaw(sprintf("FIND_IN_SET(id, '%s')", join(',', $productIds)))
+            ->get();
+
+        $pager = new LengthAwarePaginator($products, $result['hits']['total'], $perPage, $page, [
+            'path' => route('products.index', false),
+        ]);
+
+        return view('products.index', [
+            'products' => $pager,
+            'filters' => [
+                'search' => $search,
+                'order' => $order,
+            ],
+            'category' => $category ?? null,
+            'categoryTree' => $categoryService->getCategoryTree(),
+        ]);
     }
 
     public function show(Product $product, Request $request)
